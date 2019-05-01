a19a3d9a3d510df6eebe9c6f92dae06328148c85
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-05-01 00:27:49 +0000

    Snapshot of branch master taken on 2019-04-30T20:27:48.725-04:00 from "https://github.com/UnofficialJuliaMirror/Flatten.jl-4c728ea3-d9ee-5c9a-9642-b6f7d7dc04fa"

--- src/unitless.jl
+++ src/unitless.jl
@@ -1,28 +1,38 @@
 using .Unitful
 export ulflatten, ulreconstruct, ulupdate!
 
-"Unitless flattening. Flattens a nested type to a Tuple or Vector"
+
+# Unitless flatten
+
+ulflatten(action::FlattenAction, x) = flatten(action, x)
+ulflatten(::Use, x::Unitful.Quantity) = (x.val,) 
+@generated ulflatten(::Recurse, t) = flatten_inner(t, :ulflatten)
+
 ulflatten(::Type{V}, t) where V <: AbstractVector = V([ulflatten(t)...])
 ulflatten(::Type{Tuple}, t) = ulflatten(t)
-ulflatten(x::Nothing) = ()
-ulflatten(x::Number) = (x,) 
-ulflatten(x::Unitful.Quantity) = (x.val,) 
-@generated ulflatten(t) = flatten_inner(t, :ulflatten)
 
+"Unitless flattening. Flattens a nested type to a Tuple or Vector"
+ulflatten(t) = ulflatten(action(t), t)
+
+
+# Unitless reconstruct
+
+ulreconstruct(action::FlattenAction, x, data, n) = reconstruct(action, x, data, n) 
+ulreconstruct(::Use, ::T, data, n) where T <: Unitful.Quantity = (unit(T) * data[n],), n + 1
+@generated ulreconstruct(::Recurse, t, data, n) = reconstruct_inner(t, :ulreconstruct)
 
 " Reconstruct an object from partial Tuple or Vector data and another object"
-ulreconstruct(t, data) = ulreconstruct(t, data, 1)[1][1]
-ulreconstruct(::Nothing, data, n) = (nothing,), n
-ulreconstruct(::Number, data, n) = (data[n],), n + 1 
-ulreconstruct(::T, data, n) where T <: Unitful.Quantity = (unit(T) * data[n],), n + 1
-@generated ulreconstruct(t, data, n) = reconstruct_inner(t, :ulreconstruct)
+ulreconstruct(t, data) = ulreconstruct(action(t), t, data, 1)[1][1]
+
+
+# Unitless update
+
+ulupdate!(action::FlattenAction, x, data, n) = update!(action, x, data, n)
+ulupdate!(::Use, ::T, data, n) where T <: Unitful.Quantity = (unit(T) * data[n],), n + 1
+@generated ulupdate!(::Recurse, t::T, data, n) where T = update_inner(T, :ulupdate!)
 
 " Update a mutable object with partial Tuple or Vector data"
 ulupdate!(t, data) = begin
-    ulupdate!(t, data, 1)
+    ulupdate!(action(t), t, data, 1)
     t
 end
-ulupdate!(::Nothing, data, n) = (nothing,), n
-ulupdate!(::Number, data, n) = (data[n],), n + 1 
-ulupdate!(::T, data, n) where T <: Unitful.Quantity = (unit(T) * data[n],), n + 1
-@generated ulupdate!(t::T, data, n) where T = update_inner(T, :ulupdate!)
